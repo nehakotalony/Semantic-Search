973c812179684997a05bb3299dd5d35e7a4c919e
Dhanushsai36 <kotalonydhanush@gmail.com>
2025-09-16 14:23:29 +0530

    reverted changes in app.py

--- app.py
+++ app.py
@@ -5,17 +5,12 @@ from pubmed_client import search_pubmed, fetch_abstracts
 from utils import expand_query_with_synonyms
 from summarizer import generate_summary   # Hugging Face summarizer
 
-print("🚀 Starting Flask app...")
-print(f"__name__ = {__name__}")
-
 app = Flask(__name__, template_folder="templates", static_folder="static")
-print("✅ Flask app object created successfully")
-
 CORS(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html")   # This will load your frontend
 
 @app.route("/api/health")
 def health():
@@ -50,3 +45,6 @@ def search():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
